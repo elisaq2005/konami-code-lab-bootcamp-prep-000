29eb0580108013cc02cb186a79d9d919f873fadd
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-04-30 20:21:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,19 +3,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
-  document.addEventListener(e)
-  function oneKeyDownHandler(e) {
+  document.addEventListener('keydown',function(e) {
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
       index++;
       if (index === code.length) {
-        index.addEventListener('keydown',function(event) {
           alert("Hurray!");
           index = 0;
-          })
-        }
+          }
     } else {
       index = 0;
     }
-  }
-}
+  })}
