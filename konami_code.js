b341227b9299411bda7799abd29aff6e88d3ca98
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-04-30 20:12:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  let code = document.querySelectora('code');
+  let index = 0;
   function oneKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
